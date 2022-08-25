35913c624f9901f18c15eada53bffa764ecf17c9
Kamil Wolak <kamil.wolak@interia.eu>
2022-08-25 10:11:17 +0200

    Added validation for create order

--- Shop.Application/Functions/Orders/Command/CreateOrder/CreateOrderCommand.cs
+++ Shop.Application/Functions/Orders/Command/CreateOrder/CreateOrderCommand.cs
@@ -1,4 +1,6 @@
 ﻿using MediatR;
+using Shop.Domain.Entities;
+using Shop.Domain.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,9 @@ namespace Shop.Application.Functions.Orders.Command.CreateOrder
 {
     public class CreateOrderCommand : IRequest<int>
     {
+        public int BasketId { get; set; }
+        public int UserId { get; set; }
+        public Shipment Shipment { get; set; }
 
     }
 }
