ef0ef9205de3739883df90ed379e071ecd22977e
Kamil Wolak <kamil.wolak@interia.eu>
2022-12-15 20:08:02 +0100

    Added angular project

---
+++ Shop.Domain/Enums/Dto/ProductQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Persistence.EF.Dto
+{
+    public class ProductQuery
+    {
+        public string? SearchPhrase { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 2;
+        public string? SortBy { get; set; }
+        public SortDirection SortDirection { get; set; }
+    }
+}
